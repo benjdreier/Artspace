ee91ddaae5169c1d2325f28a416c326704025e89
Ben Dreier <benjdreier@gmail.com>
2018-12-03 21:38:27 -0500

    variable names

--- frontend.js
+++ frontend.js
@@ -265,19 +265,19 @@ canvas.addEventListener("touchmove", function(e){
 		// Zeeping point 2 fixed, zoom in.
 		zoomIntoPoint(Math.log2(dscale1), touch2.clientX, touch2.clientY);
 
-		// // Now for the second one, we need the updated distance and position
-		// // du stays the same (I think)
-		// // Calulate new displacement
-		// let dx2 = dx1 + vx1;
-		// let dy2 = dy1 + vy1;
-		// let distance2 = Math.sqrt(Math.pow(dx2, 2) + Math.pow(dy2, 2));
+		// Now for the second one, we need the updated distance and position
+		// du stays the same (I think)
+		// Calulate new displacement
+		let dx2 = dx1 + vx1;
+		let dy2 = dy1 + vy1;
+		let distance2 = Math.sqrt(Math.pow(dx2, 2) + Math.pow(dy2, 2));
 
-		// let mvmtAway2 = (vx2 * dux) + (vy2 * duy);
-		// let dscale2 = (mvmtAway2 / distance2) + 1;
+		let mvmtAway2 = (vx2 * dux) + (vy2 * duy);
+		let dscale2 = (mvmtAway2 / distance2) + 1;
 
-		// // Position of slightly moved touch 1 in grid coords
-		// //let pos1 = toGridCoords(touch1.clientX + vx1, touch1.clientY + vy1);
-		// zoomIntoPoint(Math.log2(dscale1), touch1.clientX, touch1.clientY);
+		// Position of slightly moved touch 1 in grid coords
+		//let pos1 = toGridCoords(touch1.clientX + vx1, touch1.clientY + vy1);
+		zoomIntoPoint(Math.log2(dscale2), touch1.clientX + vx1, touch1.clientY + vy1);
 
 		// Sure hope this works.
 		drawGrid();
